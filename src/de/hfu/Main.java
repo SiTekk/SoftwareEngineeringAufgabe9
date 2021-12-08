3fbd3009cb06b633bef8b13bcb54f59b2ed67b4b
Maximilian Maes <maximilian.maes@aesculap.de>
2021-12-08 11:51:24 +0100

    Abmelden

--- src/de/hfu/Main.java
+++ src/de/hfu/Main.java
@@ -1,28 +1,28 @@
 package de.hfu;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Nachrichtendienst n=new Nachrichtendienst();
+public class Main {
+    public static void main(String[] args) {
+        Nachrichtendienst n = new Nachrichtendienst();
 
-        Sender das_erste=new Sender("Das Erste");
-        Sender zdf=new Sender("ZDF");
+        Sender das_erste = new Sender("Das Erste");
+        Sender zdf = new Sender("ZDF");
 
-        Empfaenger empfaenger1=new Empfaenger("Maesmade");
-        Empfaenger empfaenger2=new Empfaenger("Kienzles");
+        Empfaenger empfaenger1 = new Empfaenger("Maesmade");
+        Empfaenger empfaenger2 = new Empfaenger("Kienzles");
 
         n.anmelden(empfaenger1);
         n.anmelden(empfaenger2);
 
-        Nachricht nachricht=new Nachricht(das_erste,Nachrichtentyp.Textnachricht,"Mogul wird erschossen","XY ist gestorben");
+        Nachricht nachricht = new Nachricht(das_erste, Nachrichtentyp.Textnachricht, "Mogul wird erschossen", "XY ist gestorben");
 
-        Nachricht nachricht1=new Nachricht(zdf,Nachrichtentyp.Multimedianachricht,"Feuerwerk brennt Haus nieder","https://player.vimeo.com/video/226635322?title=0&portrait=0&byline=0&autoplay=1&loop=1&transparent=1");
+        Nachricht nachricht1 = new Nachricht(zdf, Nachrichtentyp.Multimedianachricht, "Feuerwerk brennt Haus nieder", "https://player.vimeo.com/video/226635322?title=0&portrait=0&byline=0&autoplay=1&loop=1&transparent=1");
 
-        n.send(nachricht,"Maesmade");
+        n.send(nachricht, "Maesmade");
 
         n.send(nachricht1);
 
+        n.abmelden(empfaenger1);
+        n.abmelden(empfaenger2);
 
 
     }
