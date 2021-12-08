77b560f3e9719e56da6fca9975be1062c1447ff7
Maximilian Maes <maximilian.maes@aesculap.de>
2021-12-08 11:12:21 +0100

    Added implementierung Anmelden/ Abmelden und send(n)

--- src/de/hfu/Nachrichtendienst.java
+++ src/de/hfu/Nachrichtendienst.java
@@ -4,43 +4,39 @@ import java.util.*;
 
 public class Nachrichtendienst {
 
-    private List<Empfaenger> angemeldeteEmpfaenger;
+    private List<Empfaenger> angemeldeteEmpfaenger = new ArrayList<>();
 
     /**
-     *
      * @param nachricht
      * @param benutzername
      */
     public void send(Nachricht nachricht, String benutzername) {
         // TODO - implement Nachrichtendienst.send
         throw new UnsupportedOperationException();
+
     }
 
     /**
-     *
      * @param nachricht
      */
     public void send(Nachricht nachricht) {
-        // TODO - implement Nachrichtendienst.send
-        throw new UnsupportedOperationException();
+        for (Empfaenger empfaenger : angemeldeteEmpfaenger) {
+            empfaenger.empfangen(nachricht);
+        }
     }
 
     /**
-     *
      * @param empfaenger
      */
     public void anmelden(Empfaenger empfaenger) {
-        // TODO - implement Nachrichtendienst.anmelden
-        throw new UnsupportedOperationException();
+        angemeldeteEmpfaenger.add(empfaenger);
     }
 
     /**
-     *
      * @param empfaenger
      */
     public void abmelden(Empfaenger empfaenger) {
-        // TODO - implement Nachrichtendienst.abmelden
-        throw new UnsupportedOperationException();
+        angemeldeteEmpfaenger.remove(empfaenger);
     }
 
 }
